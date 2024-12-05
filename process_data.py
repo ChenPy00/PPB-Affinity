6449d99938dbc5342beb45cf2737049fc5b44099
Peiyi Chen <453225733@qq.com>
2024-12-05 16:59:36 +0800

    Calculate the dG value through the "KD(M)" column

--- process_data.py
+++ process_data.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 import pandas as pd
+import numpy as np
 import os
 
-data_dir = '../PPB-Affinity-DataPrepWorkflow/processed_data'
+data_dir = 'Your PPB-Affinity data path'  # Such as './PPB-Affinity'
 path_dict = {
-    'SKEMPI v2.0': './PDB/SKEMPI v2.0/',
-    'PDBbind v2020': './PDB/PDBbind v2020/',
-    'SAbDab': './PDB/SAbDab/',
-    'ATLAS': './PDB/ATLAS/',
-    'Affinity Benchmark v5.5': './PDB/Affinity Benchmark v5.5/',
+    'SKEMPI v2.0': '/PDB/SKEMPI v2.0/',
+    'PDBbind v2020': '/PDB/PDBbind v2020/',
+    'SAbDab': '/PDB/SAbDab/',
+    'ATLAS': '/PDB/ATLAS/',
+    'Affinity Benchmark v5.5': '/PDB/Affinity Benchmark v5.5/',
 }
 for k,v in path_dict.items(): path_dict[k] = os.path.join(data_dir,v)
 
@@ -31,7 +32,7 @@ def create_pdb_path(inputs):
     file_name = eval(f'pdb_code.{case}()') + suffix
     pdb_path = os.path.join(path_dict[source], file_name)
     if os.path.exists(pdb_path):
-        return pdb_path# os.path.realpath(pdb_path)
+        return pdb_path  # os.path.realpath(pdb_path)
     else:
         import pdb
         pdb.set_trace()
@@ -46,15 +47,15 @@ def mutstr_transform(mutstr):
     return mutstr
 
 
-affinity_data = pd.read_excel('../PPB-Affinity-DataPrepWorkflow/processed_data/PPB-Affinity.xlsx',
+affinity_data = pd.read_excel('PPB-Affinity execl file path',  # Such as os.path.join(data_dir, 'PPB-Affinity.xlsx')
                               usecols=['PDB', 'Source Data Set', 'Mutations', 'Ligand Chains', 'Receptor Chains',
-                                       'dG(kcal/mol)', 'Subgroup'],
+                                        'KD(M)', 'Subgroup'],
                               dtype={"PDB": str})
-affinity_data.rename(columns={'Source Data Set': 'source', 'dG(kcal/mol)': 'dG',
+affinity_data.rename(columns={'Source Data Set': 'source', 'KD(M)': 'KD',
                               'Mutations': 'mutstr', 'PDB': 'pdb',
                               'Ligand Chains': 'ligand', 'Receptor Chains': 'receptor'}, inplace=True)
 
-
+affinity_data['dG'] = (8.314/4184)*(273.15 + 25.0) * np.log(affinity_data['KD'])
 affinity_data = affinity_data[~(affinity_data.ligand.str.len() + affinity_data.receptor.str.len() > 26)]
 affinity_data.reset_index(drop=True, inplace=True)
 affinity_data['pdb_path'] = affinity_data[['pdb', 'source']].apply(create_pdb_path, axis=1)
